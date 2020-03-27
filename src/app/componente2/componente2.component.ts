e84eae8b8a86ca6f44368a57fa3c2bc35d2b7d87
jeinostroza <jeinostroza@hotmail.com>
2020-03-27 15:10:28 -0500

    First commit

---
+++ src/app/componente2/componente2.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-componente2',
+  templateUrl: './componente2.component.html',
+  styleUrls: ['./componente2.component.css']
+})
+export class Componente2Component implements OnInit {
+
+  constructor(private route: ActivatedRoute,  private router:Router ) { }
+
+  ngOnInit() {
+
+    this.route.params.subscribe(params =>{
+      if (params["id"]!= null){
+        console.log("Llego con el paramentro id " + params["id"]);
+      }
+    })
+  }
+
+}
